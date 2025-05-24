5c69bd3146ecd6b9330d64368de87555f6798535
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-05-24 08:50:40 +0300

    Create 04_SUM_OF_ASCII.py

---
+++ LABS/02_DATA_TYPES/IVAN_SOLUTIONS/04_SUM_OF_ASCII.py
@@ -0,0 +1,8 @@
+number_of_characters = 0
+total_sum = 0
+
+for char in range(number_of_characters):
+    current_char = input()
+    total_sum += ord(current_char)
+
+print(f"The sum equals: {total_sum}".)
