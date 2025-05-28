dcb48bfbc2a4cd33573d8f09c3f89659d6477cb9
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-05-28 19:28:16 +0300

    NOTES FROM LECTURE #3

---
+++ LABS/02_DATA_TYPES/LECTURES/03_LIST_BASICS/list_join.py
@@ -0,0 +1,10 @@
+#creates a string from list elements
+user_data = input().split() #input: "John Doe"
+
+fist_name,last_name = user_data  #first_name = "John", last_name = "Doe"
+print(f"{fist_name}\n{last_name}")
+
+#this is called list unpacking, when you unpack a list index into multiple variables
+
+user_names = " ".join(user_data)  #user_names = "John Doe"
+print(user_names)  #prints: John Doe
