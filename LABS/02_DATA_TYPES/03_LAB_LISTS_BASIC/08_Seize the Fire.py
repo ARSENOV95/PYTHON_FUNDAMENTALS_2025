a19c08623bef887eb8991dee399972c923bd82c8
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-05-31 12:31:27 +0300

    Update 08_Seize the Fire.py

--- LABS/02_DATA_TYPES/03_LAB_LISTS_BASIC/08_Seize the Fire.py
+++ LABS/02_DATA_TYPES/03_LAB_LISTS_BASIC/08_Seize the Fire.py
@@ -1,31 +1,33 @@
-fire_cells = input().split("#")
-current_water_amount = int(input())
+fire_cells = input().split("#") #input the fumber fo fire cells 
+current_water_amount = int(input()) #total amaout of letf water for the party
 
-effort = 0 
-total_fire = 0
+effort = 0  #the total amount of efferdto be calculated
+total_fire = 0 #the total amout of 
 
 
-print("Cells:")
-for cell in fire_cells:
-    current_cell = cell.split(" = ")
-    fire_type = current_cell[0]
-    water_needed = int(current_cell[1])
+print("Cells:") #prints the cell part of the text
+for cell in fire_cells:  # a loop to iterate trough all cells
+    current_cell = cell.split(" = ") # a list ot store the split elements of the cell
+    fire_type = current_cell[0] # a variable to take the type of the cell
+    water_needed = int(current_cell[1]) # the amout of watter needed to put out the cell
 
-    if current_water_amount < water_needed:
+    if current_water_amount < water_needed: # if the amount neeeded is bgger iy should put put the cell
         continue
 
-    if (fire_type == "High" and  81 <= water_needed <= 125) or \
-        (fire_type == "Medium" and  51 <= water_needed <= 80) or \
+    if current_water_amount <= 0:
+        break
+    
+    #if the cell is one of the three types it's value should be prited, effrod calculated and total fire added 
+    if (fire_type == "High" and  81 <= water_needed <= 125) or\
+        (fire_type == "Medium" and  51 <= water_needed <= 80) or\
         (fire_type == "Low" and  1 <= water_needed <= 50):
         effort += water_needed * 0.25
         current_water_amount -= water_needed
         total_fire += water_needed
-        print(f"- {water_needed}")
+        print(f"- {water_needed}") #at the end print each cell in the correct format  
 
 
 
-
-    
 print(f"Effort: {effort:.2f}")
 print(f"Total Fire: {total_fire}")
 
