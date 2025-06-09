e1a8dfc0b8bbb4ca657967cdf110ffff7db079e0
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-06-09 14:52:49 +0300

    Create 05.py

---
+++ LABS/02_DATA_TYPES/04_FUNCTIONS/IVAN_SOLUTIONS/05.py
@@ -0,0 +1,12 @@
+def even(n :int) -> bool: 
+    return  n %2 == 0
+
+numbers_as_string  = input().split()
+numbers_as_digits  = []
+
+
+for number in numbers_as_string:
+    numbers_as_digits.append(int(number))
+
+final_result = list(filter(even,numbers_as_digits))
+print(final_result)
