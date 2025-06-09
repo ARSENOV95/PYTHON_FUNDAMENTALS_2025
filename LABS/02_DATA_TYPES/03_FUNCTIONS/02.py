5b534c213cdac9a733d99687dd3747caab618439
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-06-09 06:39:21 +0300

    changes in lab 4

---
+++ LABS/02_DATA_TYPES/03_FUNCTIONS/02.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+
+def sum_numbers(num1,num2):
+    return num1 + num2
+
+def subtract():
+    return sum_numbers(num1,num2) - num3
+
+print(subtract())
