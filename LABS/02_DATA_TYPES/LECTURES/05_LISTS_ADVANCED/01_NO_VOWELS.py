beb8b9a8af17591d381f5cca8ec025f767d7f015
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-06-10 16:32:31 +0300

    Update 01_NO_VOWELS.py

--- LABS/02_DATA_TYPES/LECTURES/05_LISTS_ADVANCED/01_NO_VOWELS.py
+++ LABS/02_DATA_TYPES/LECTURES/05_LISTS_ADVANCED/01_NO_VOWELS.py
@@ -1,11 +1,12 @@
-def no_vowels(words :str)->str:
+def vowels_check(words :str)->str:
 
     vowels = ['a','e','i','o','u'] # a list of volew which we used for checking each letter form the entered word 
     n_vowels = [letter for letter in words if letter.lower() not in vowels]  
     #append a letter from a string when it's lowercase conversion is not in sthe string of vowels 
+    #why,  beacuse if we convert the letter to lowe in the if condition we can make it case insensitive 
 
     return print("".join(n_vowels)) #the result should be a print 
 
 string = input()
 
-no_vowels(string)
+vowels_check(string)
