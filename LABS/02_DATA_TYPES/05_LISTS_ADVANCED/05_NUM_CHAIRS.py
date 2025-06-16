6e05aa0e6a43ac30cae1d027057bc9965397c0ff
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-06-16 07:10:13 +0300

    Update 05_NUM_CHAIRS.py

--- LABS/02_DATA_TYPES/05_LISTS_ADVANCED/05_NUM_CHAIRS.py
+++ LABS/02_DATA_TYPES/05_LISTS_ADVANCED/05_NUM_CHAIRS.py
@@ -1,19 +1,22 @@
 def num_chairs(num_rooms):
+    needed = 0
     free_chairs = 0
     
     for room in range(1,num_rooms + 1):
-        chairs_needed = 0
+        chairs_needed_per_room = 0
         
         current_room = input().split(" ")
 
-        if len(current_room[0]) < int(current_room[1]):
-            chairs_needed += int(current_room[1]) - len(current_room[0])
-            print(f"{chairs_needed} more chairs needed in room {room}")
+        if len(current_room[0]) < int(current_room[1]): 
+            chairs_needed_per_room += int(current_room[1]) - len(current_room[0])
+            print(f"{chairs_needed_per_room} more chairs needed in room {room}")
         
         elif len(current_room[0]) > int(current_room[1]):
             free_chairs += len(current_room[0]) - int(current_room[1])
 
-    if free_chairs > 0:
+        needed += chairs_needed_per_room
+
+    if free_chairs - needed > 0:
         return f"Game On, {free_chairs} free chairs left"
 
         
