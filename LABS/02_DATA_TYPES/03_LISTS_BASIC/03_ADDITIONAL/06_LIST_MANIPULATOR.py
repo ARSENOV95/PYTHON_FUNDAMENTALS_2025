d720d8576e56bcc4736e43e689f1e961f1139d7d
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-06-25 17:01:40 +0300

    Update 06_LIST_MANIPULATOR.py

--- LABS/02_DATA_TYPES/03_LISTS_BASIC/03_ADDITIONAL/06_LIST_MANIPULATOR.py
+++ LABS/02_DATA_TYPES/03_LISTS_BASIC/03_ADDITIONAL/06_LIST_MANIPULATOR.py
@@ -2,13 +2,12 @@ list_nums = list(map(int,input().split()))
 
 while (command := input()) != "end":
     args = command.split()[1:]
-    invalid = False 
 
     if command.startswith("exchange"):
         index_split_val = int(args[0])
 
-        if 0<= index_split_val < len(list_nums):
-            first_half = list_nums[:index_split_val+1]
+        if index_split_val in range(len(list_nums)):
+            first_half = list_nums[:index_split_val+ 1]
             second_half = list_nums[index_split_val+1:]
             list_nums = second_half + first_half
         
@@ -26,11 +25,13 @@ while (command := input()) != "end":
             continue
 
         val = max(even_odd) if command.startswith("max") else min(even_odd)
+        max_vals = []
 
         for idx in range(len(list_nums) -1,-1,-1):
             if list_nums[idx] == val:
-               print(idx)
-               break
+               max_vals.append(idx)
+        
+        print(max_vals[0])
 
     elif command.startswith("first") or command.startswith("last"):
         count = int(args[0])
@@ -46,10 +47,12 @@ while (command := input()) != "end":
 
         if not even_odd:
             print([])
+
         else:
             count_nums = even_odd[0:count] if command.startswith("first") else even_odd[count:]
+            if count > len(even_odd):
+                count_nums = even_odd
             print(count_nums)
-
 
 else:
     print(list_nums)
