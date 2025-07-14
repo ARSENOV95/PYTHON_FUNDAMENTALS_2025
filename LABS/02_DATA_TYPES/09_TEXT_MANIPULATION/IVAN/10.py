07d5c0710ddfedb1cb23853b8b482a8cad17f7a3
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-07-14 07:34:41 +0300

    Update 10.py

--- LABS/02_DATA_TYPES/09_TEXT_MANIPULATION/IVAN/10.py
+++ LABS/02_DATA_TYPES/09_TEXT_MANIPULATION/IVAN/10.py
@@ -1,19 +1,19 @@
 def check_ticket (ticket:str)->str:
     if len(ticket) != 20:
-        return "Invalid ticket" #if the 20 <ticket < 21 return Ivnalid ticket else continei function 
+        return "invalid ticket" #if the 20 <ticket < 21 return Ivnalid ticket else continei function 
 
-    match_symbol = ('@', '#','$','^')
+    winning_symbol = ('@', '#','$','^')
     left_part = ticket[:10]
-    right_part = ticket[:10]
+    right_part = ticket[10:]
 
-    for winning_symbol in match_symbol:
+    for match_symbol in winning_symbol:
         for uninterurupted_match_lenght  in range(10,5,-1): #iterratetews torugh the  possible repetiton for winning cases
             winning_symbol_reptition =  match_symbol * uninterurupted_match_lenght
             #$$$$$$$$ or #$$$$$$$
             if winning_symbol_reptition in left_part and\
             winning_symbol_reptition in right_part:
                 if uninterurupted_match_lenght == 10:
-                    return f'ticket "{ticket}"  - {uninterurupted_match_lenght}{winning_symbol} Jackpot!'
+                    return f'ticket "{ticket}" - {uninterurupted_match_lenght}{match_symbol} Jackpot!'
                 return f'ticket "{ticket}" - {uninterurupted_match_lenght}{match_symbol}'
     # if there is no winning symobl in both half  we exti the loo pand return no match
     return f'ticket "{ticket}" - no match' # if no winning symbol but valid - no match
