fff100f04aaad07eea034d4941fcdc1bf01baafe
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-08-21 23:12:44 +0300

    Update 04_BATTLE_SHIPS.py

--- LABS/02_DATA_TYPES/05_LISTS_ADVANCED/ADVANCED/04_BATTLE_SHIPS.py
+++ LABS/02_DATA_TYPES/05_LISTS_ADVANCED/ADVANCED/04_BATTLE_SHIPS.py
@@ -1,12 +1,20 @@
 num_rows = int(input())
 
 grid = []
+destroyed_ships = 0
 
 for i in range(num_rows):
     coordinates = list(map(int,input().split()))
 
     grid.append(coordinates)
 
-print(grid)
+#print(grid)
 
-attackes = input().split()
+attacks = input().split()
+
+print(attacks)
+
+for attack in attacks:
+    x,y = map(int,attack.split("-"))
+    print(x,y)
+    
