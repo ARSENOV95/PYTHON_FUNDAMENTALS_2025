d65702cbb589f52306e94c22dab16bd35c34dc46
ARSENOV95 <a.v.arsenov95@gmail.com>
2025-08-26 07:03:18 +0300

    Update TRIBONACII_SEQUENCE.py

--- LABS/02_DATA_TYPES/04_FUNCTIONS/04_ADDITIONAL/TRIBONACII_SEQUENCE.py
+++ LABS/02_DATA_TYPES/04_FUNCTIONS/04_ADDITIONAL/TRIBONACII_SEQUENCE.py
@@ -1,11 +1,18 @@
-def tribunacii_sequence(num :int)->str:
-    list_of_numbers = []
-    
-    for i in range(num):
-        number = sum(list_of_numbers[-3:])
-        list_of_numbers.append(number)
-    return print(' '.join([str(char) for char in list_of_numbers]))
+def tribunacii_sequence(nums :int)->list:
 
-number_of_digits = int(input())
+    sequence = [1] # first number of the sequence is always one 
 
-tribunacii_sequence(number_of_digits)
+    #for the lenght of the sequence loop 
+    for i in range(nums - 1):  #since we ahave the first number will loop trought the len of the sequence - 1
+        if len(sequence) < 3: # if the len is < 3 the next sequence will be the sum of the whole list
+            next_num = sum (sequence)
+        else: #else it will be the um of the last 3 numbers 
+            next_num = sum (sequence[-3:])
+        
+        sequence.append(next_num)
+
+    return ' '.join([str(x) for x in sequence]) 
+
+seq_len = int(input())
+
+print(tribunacii_sequence(seq_len))
